6eb714924a9eff465636884141d1b15fbdb3fe6f
Niwa Kazutomo <niwa_kazutomo@gs2.io>
2020-12-21 16:40:01 +0900

    String型を作成し、JSON形式のデータを受け取れるように

--- core/model.go
+++ core/model.go
@@ -139,9 +139,6 @@ func (p *String) MarshalJSON() ([]byte, error) {
 	if p == nil {
 		return nil, nil
 	}
-	if len(*p) == 0 {
-		return []byte(""), nil
-	}
-	return []byte(*p), nil
+	return []byte("\"" + *p + "\""), nil
 }
 
